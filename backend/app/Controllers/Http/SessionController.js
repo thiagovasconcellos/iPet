b658589b86071404fb6aa6b0aeedef138d3138cd
thiagovasconcellos <thiagovasconcellos88@gmail.com>
2020-04-17 16:21:48 -0300

    minor fixes

--- backend/app/Controllers/Http/SessionController.js
+++ backend/app/Controllers/Http/SessionController.js
@@ -4,17 +4,28 @@
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 /** @typedef {import('@adonisjs/auth/src/Schemes/Session')} AuthSession */
 
+const User = use('App/Models/User')
+
 class SessionController {
   /**
    * @param {Request} ctx.request
    * @param {AuthSession} ctx.auth
    */
-  async store ({ request, response, auth }) {
+  async store({ request, response, auth }) {
     const { email, password } = request.all()
 
     const token = await auth.attempt(email, password)
 
-    return response.created(token)
+    const user = await User.findBy('email', email)
+
+    console.log(JSON.stringify(user))
+
+    const logged = {
+      token: token.token,
+      user
+    }
+
+    return response.json(logged)
   }
 }
 
